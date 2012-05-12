f29f2d6233b4525e4412bd9de5bc6b436cc4ce7c
Abbas Adel <abbas.adel@mufix.org>
2012-05-12 23:45:16 +0200

    use addthis and open graph

--- hooks/shareit.php
+++ hooks/shareit.php
@@ -30,8 +30,11 @@ class ShareIt {
 		// Only add the events if we are on that controller
 		if (Router::$controller == 'reports' AND Router::$method == 'view')
 		{
-			Event::add('ushahidi_action.report_extra', array($this, 'embed_facebook'));
-			Event::add('ushahidi_action.report_extra', array($this, 'embed_twitter'));
+			Event::add('ushahidi_action.report_extra', array($this, 'embed_addthis'));
+			//Event::add('ushahidi_action.report_extra', array($this, 'embed_twitter'));
+			Event::add('ushahidi_action.report_display_media', array($this, 'open_graph'));
+			
+			
 			// Event::add('ushahidi_action.main_sidebar', array($this, 'hello'));
 			
 			// Overwrite current comments block and comments form block
@@ -40,20 +43,96 @@ class ShareIt {
 		}
 	}
 	
+	public function embed_addthis(){
+		$view = View::factory('addthis');
+		$view->render(TRUE);
+	}
+	
 	public function embed_facebook()
 	{
 		$view = View::factory('facebook-share');
 		$view->facebook_app_id = Kohana::config('facebook-social.facebook_api_key');
-		//$view->xd_receiver = url::site()."xd_receiver";
 		$view->render(TRUE);
 	}
 	
 	public function embed_twitter()
 	{
 		$view = View::factory('twitter-share');
-		//$view->facebook_app_id = Kohana::config('facebook-social.facebook_api_key');
-		//$view->xd_receiver = url::site()."xd_receiver";
 		$view->render(TRUE);
+	}
+	
+	
+	public function open_graph(){
+		
+		$id = Event::$data;
+		if ($id > 0 AND Incident_Model::is_valid_incident($id,TRUE))
+		{
+			$incident = ORM::factory('incident')
+				->where('id',$id)
+				->where('incident_active',1)
+				->find();
+
+			if ( ! $incident->loaded){
+				return;
+			}
+		}
+		
+		
+		//media variables
+		$incident_news = array();
+		$incident_video = array();
+		$incident_photo = array();
+		
+		foreach($incident->media as $media)
+			{
+				if ($media->media_type == 4)
+				{
+					$incident_news[] = $media->media_link;
+				}
+				elseif ($media->media_type == 2)
+				{
+					$incident_video[] = $media->media_link;
+				}
+				elseif ($media->media_type == 1)
+				{
+					$incident_photo[] = array(
+						'large' => url::convert_uploaded_to_abs($media->media_link),
+						'thumb' => url::convert_uploaded_to_abs($media->media_thumb)
+						);
+				}
+			}
+		
+		$og = array(
+			'og:title' => $incident->incident_title,
+			'og:url' => url::site() . 'reports/view/'.$id,
+			'og:site_name' => Kohana::config('settings.site_name'),
+		);		
+		
+		//create head meta markup
+		$markup = '';
+		echo '<script>';
+		foreach($og as  $key => $value){
+			$markup .= '<meta property="'.$key.'" content="'.$value.'" />';
+		}
+		
+		
+		//add image to head meta
+		foreach ($incident_photo as $photo) {
+			$markup .= '<meta property="og:image" content="'.$photo['large'].'" />';
+		}
+		
+		//add video to head meta
+		foreach ($incident_video as $video) {
+			$markup .= '<meta property="og:video" content="'.$video.'" />';
+		}
+		
+		echo "$('head').append('$markup');";
+		echo '</script>';
+	}
+	
+	
+	public function scripts(){
+		echo 'opengraph script ' . time();
 	}
 }
 
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 name: Shareit
 website: http://www.ushahidi.com
 description: Allow users to Share reports
-version: 0.1
+version: 0.2
 requires: 2.x
 tested up to: 2.x
 author: Abbas Ael
@@ -15,4 +15,6 @@ author website: http://www.zabatak.com
 == Installation ==
 
 == Changelog ==
-
+0.2 --
+1. Use addthis
+2. add open graph to head
